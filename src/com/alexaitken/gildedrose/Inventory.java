7096092cd9b8440f1b14cc13609e01924111e4fd
Julio García <jjgarcia@zentyal.com>
2013-04-21 18:31:28 +0200

    Solution done in the Coding Dojo 12 of April

--- src/com/alexaitken/gildedrose/Inventory.java
+++ src/com/alexaitken/gildedrose/Inventory.java
@@ -2,6 +2,9 @@ package com.alexaitken.gildedrose;
 
 public class Inventory {
 
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 	private Item[] items;
 
 	public Inventory(Item[] items) {
@@ -11,68 +14,77 @@ public class Inventory {
 
 	public Inventory() {
 		super();
-		items = new Item[] {
-					new Item("+5 Dexterity Vest", 10, 20), 
-					new Item("Aged Brie", 2, 0),
-					new Item("Elixir of the Mongoose", 5, 7),
-					new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-					new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-					new Item("Conjured Mana Cake", 3, 6) 
-				};
+		items = new Item[] { new Item("+5 Dexterity Vest", 10, 20),
+				new Item(AGED_BRIE, 2, 0),
+				new Item("Elixir of the Mongoose", 5, 7),
+				new Item(SULFURAS_HAND_OF_RAGNAROS, 0, 80),
+				new Item(BACKSTAGE_PASSES, 15, 20),
+				new Item("Conjured Mana Cake", 3, 6) };
 
 	}
 
 	public void updateQuality() {
+
 		for (int i = 0; i < items.length; i++) {
-			if (items[i].getName() != "Aged Brie"
-					&& items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-				if (items[i].getQuality() > 0) {
-					if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-						items[i].setQuality(items[i].getQuality() - 1);
-					}
+			Item item = items[i];
+
+			if (item.getName().equals("Conjured")) {
+				updateItem(item, 2);
+			}
+			else if (item.getName().equals(AGED_BRIE)) {
+				updateItemAgedBrie(item);
+			}
+			else if (item.getName().equals(SULFURAS_HAND_OF_RAGNAROS)) {
+				updateItemSulfuras(item);
+			}
+			else if (item.getName().equals(BACKSTAGE_PASSES)) {
+
+				items[i].setQuality(items[i].getQuality() + 1);
+
+				if (items[i].getSellIn() < 11) {
+					items[i].setQuality(items[i].getQuality() + 1);
 				}
-			} else {
-				if (items[i].getQuality() < 50) {
+
+				if (items[i].getSellIn() < 6) {
 					items[i].setQuality(items[i].getQuality() + 1);
 
-					if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
-						if (items[i].getSellIn() < 11) {
-							if (items[i].getQuality() < 50) {
-								items[i].setQuality(items[i].getQuality() + 1);
-							}
-						}
-
-						if (items[i].getSellIn() < 6) {
-							if (items[i].getQuality() < 50) {
-								items[i].setQuality(items[i].getQuality() + 1);
-							}
-						}
-					}
 				}
-			}
+				if (items[i].getQuality() > 50) {
+					items[i].setQuality(50);
+				}
 
-			if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
 				items[i].setSellIn(items[i].getSellIn() - 1);
-			}
 
-			if (items[i].getSellIn() < 0) {
-				if (items[i].getName() != "Aged Brie") {
-					if (items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-						if (items[i].getQuality() > 0) {
-							if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-								items[i].setQuality(items[i].getQuality() - 1);
-							}
-						}
-					} else {
-						items[i].setQuality(items[i].getQuality()
-								- items[i].getQuality());
-					}
-				} else {
-					if (items[i].getQuality() < 50) {
-						items[i].setQuality(items[i].getQuality() + 1);
-					}
+				if (items[i].getSellIn() < 0) {
+					items[i].setQuality(0);
 				}
 			}
+			else {
+				updateItem(item, 1);
+			}
+
 		}
+	}
+
+	private void updateItem(Item item, int decreaseFactor) {
+		item.setSellIn(item.getSellIn() - 1);
+
+		if (item.getSellIn() < 0) {
+			decreaseFactor = decreaseFactor * 2;
+		}
+		item.setQuality(item.getQuality() - decreaseFactor);
+		if (item.getQuality() < 0) {
+			item.setQuality(0);
+		}
+	}
+
+	private void updateItemAgedBrie(Item item) {
+		item.setSellIn(item.getSellIn() - 1);
+		if (item.getQuality() < 50) {
+			item.setQuality(item.getQuality() + 1);
+		}
+	}
+
+	private void updateItemSulfuras(Item item) {
 	}
 }
--- test/test/com/alexaitken/gildedrose/InventoryTest.java
+++ test/test/com/alexaitken/gildedrose/InventoryTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.alexaitken.gildedrose.Inventory;
@@ -17,167 +18,129 @@ public class InventoryTest {
 //	new Item("Sulfuras, Hand of Ragnaros", 0, 80),
 //	new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
 //	new Item("Conjured Mana Cake", 3, 6)
+
+	private Item sulfuras;
+	private Item normalItem;
+	private Item normalItemNoQuality;
+	private Item normalItemStaled;
+	private Item agedBrie;
+	private Item agedBrieTopQuality;
+	private Item backStagePass;
+	
+	private Inventory createInventory (Item... items) {
+		return new Inventory(items);
+	}
+
+	
+	@Before
+	public void setUp() {
+		sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);		
+		normalItem = new Item("+5 Dexterity Vest", 10, 20);
+		normalItemNoQuality = new Item("+5 Dexterity Vest", 10, 0);
+		normalItemStaled = new Item("+5 Dexterity Vest", -1, 25);
+		agedBrie = new Item("Aged Brie", 10, 25);
+		agedBrieTopQuality = new Item("Aged Brie", 10, 50);
+		backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20);
+	}
 	
 	@Test
 	public void should_never_changes_quailty_of_Sulfuras() throws Exception {
-		Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
-		
-		Inventory sut = new Inventory((Item[]) Arrays.asList(new Item("Sulfuras, Hand of Ragnaros", 0, 80)).toArray());
-		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(sulfuras).toArray());
 		sut.updateQuality();
-		
 		assertEquals(80, sulfuras.getQuality());
 		
 		
 	}
 	@Test
 	public void should_never_changes_sellIn_of_Sulfuras() throws Exception {
-		Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
-		
 		Inventory sut = new Inventory((Item[]) Arrays.asList(sulfuras).toArray());
-		
 		sut.updateQuality();
-		
 		assertEquals(0, sulfuras.getSellIn());
-		
-		
 	}
 	
 	
 	
 	@Test
 	public void should_lower_the_sellIn_by_one_for_normal_items() throws Exception {
-		Item normalItem = new Item("+5 Dexterity Vest", 10, 20);
-		
 		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
-		
 		sut.updateQuality();
-		
-		
 		assertEquals(9, normalItem.getSellIn());
 	}
 	
 	@Test
 	public void should_lower_the_quality_by_one_for_normal_items() throws Exception {
-		Item normalItem = new Item("+5 Dexterity Vest", 10, 20);
-		
 		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
-		
 		sut.updateQuality();
-		
-		
 		assertEquals(19, normalItem.getQuality());
 	}
 	
 	@Test
 	public void should_not_lower_the_quality_below_zero() throws Exception {
-		Item normalItem = new Item("+5 Dexterity Vest", 10, 0);
-		
-		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
-		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItemNoQuality).toArray());
 		sut.updateQuality();
-		
-		
-		assertEquals(0, normalItem.getQuality());
+		assertEquals(0, normalItemNoQuality.getQuality());
 	}
 	
 	@Test
 	public void should_lower_the_quality_twice_as_fast_once_the_sell_in_date_has_passed() throws Exception {
-		Item normalItem = new Item("+5 Dexterity Vest", -1, 25);
-		
-		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
-		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItemStaled).toArray());
 		sut.updateQuality();
-		
-		
-		assertEquals(23, normalItem.getQuality());
+		assertEquals(23, normalItemStaled.getQuality());
 	}
-	
 	
 	@Test
 	public void should_increase_the_quality_of_aged_brie_as_it_gets_older() throws Exception {
-		Item agedBrie = new Item("Aged Brie", 10, 25);
-		
 		Inventory sut = new Inventory((Item[]) Arrays.asList(agedBrie).toArray());
-		
 		sut.updateQuality();
-		
-		
 		assertEquals(26, agedBrie.getQuality());
-	}
-	
+	}	
 	
 	@Test
 	public void should_not_increase_the_quality_of_aged_brie_over_50() throws Exception {
-		Item agedBrie = new Item("Aged Brie", 10, 50);
-		
-		Inventory sut = new Inventory((Item[]) Arrays.asList(agedBrie).toArray());
-		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(agedBrieTopQuality).toArray());
 		sut.updateQuality();
-		
-		
-		assertEquals(50, agedBrie.getQuality());
+		assertEquals(50, agedBrieTopQuality.getQuality());
 	}
 	
 	@Test
 	public void should_lower_backstage_passes_to_zero_quality_once_concert_has_happened() throws Exception {
-		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20);
-		
 		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
-		
 		sut.updateQuality();
-		
-		
 		assertEquals(0, backStagePass.getQuality());
 	}
 	
 	@Test
 	public void should_increase_backstage_passes_quality_by_1_when_the_concert_is_more_than_10_days_away() throws Exception {
-		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
-		
-		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
-		
-		sut.updateQuality();
-		
-		
+		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());		
+		sut.updateQuality();		
 		assertEquals(21, backStagePass.getQuality());
 	}
 	
 	@Test
 	public void should_increase_backstage_passes_quality_by_2_when_the_concert_is_10_days_or_less_away() throws Exception {
-		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 27);
-		
-		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
-		
+		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 27);		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());		
 		sut.updateQuality();
-		
-		
 		assertEquals(29, backStagePass.getQuality());
 	}
 	
 	@Test
 	public void should_increase_backstage_passes_quality_by_3_when_the_concert_is_5_days_or_less_away() throws Exception {
-		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 44);
-		
-		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
-		
-		sut.updateQuality();
-		
-		
+		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 44);		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());		
+		sut.updateQuality();		
 		assertEquals(47, backStagePass.getQuality());
 	}
 	
 	@Test
 	public void should_not_increase_backstage_passes_above_a_quality_of_50() throws Exception {
 		Item backStagePassMoreThan10DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50);
-		
 		Item backStagePass10DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49);
 		Item backStagePass5DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48);
-		
 		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePassMoreThan10DaysAway, backStagePass10DaysAway, backStagePass5DaysAway).toArray());
-		
+	
 		sut.updateQuality();
-		
 		
 		assertEquals(50, backStagePassMoreThan10DaysAway.getQuality());
 		assertEquals(50, backStagePass10DaysAway.getQuality());
@@ -185,9 +148,38 @@ public class InventoryTest {
 	}
 	
 	
+	@Test
+	public void should_products_conjured_twice_fastAs() {
+		Item conjuredItem = new Item ("Conjured", 5, 10);
+		Inventory inventoryConjured = createInventory(conjuredItem);
+		
+		inventoryConjured.updateQuality();
+		
+		assertEquals(8, conjuredItem.getQuality());
+		assertEquals(4, conjuredItem.getSellIn());
+	}
 	
+	@Test
+	public void should_products_conjured_four_timeas_as_fast_if_staled() {
+		Item conjuredItem = new Item ("Conjured", 0, 10);
+		Inventory inventoryConjured = createInventory(conjuredItem);
+		
+		inventoryConjured.updateQuality();
+		
+		assertEquals(6, conjuredItem.getQuality());
+		assertEquals(-1, conjuredItem.getSellIn());
+	}
 	
-	
-	
+	@Test
+	public void should_products_conjured_not_lower_the_quality_below_zero() {
+		Item conjuredItem = new Item ("Conjured", 0, 0);
+		Inventory inventoryConjured = createInventory(conjuredItem);
+		
+		inventoryConjured.updateQuality();
+		
+		assertEquals(0, conjuredItem.getQuality());
+		assertEquals(-1, conjuredItem.getSellIn());
+	}
+
 	
 }
